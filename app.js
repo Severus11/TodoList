a91e88abe56efac83459f7eaaaa001e268c9f883
Severus11 <36445722+Severus11@users.noreply.github.com>
2021-07-06 22:30:35 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -3,38 +3,25 @@ const bodyParser = require("body-parser")
 
 const app = express()
 
+app.use(bodyParser.urlencoded({extended: true}))
+
 app.set('view engine', 'ejs')
 app.get("/", function(req,res){
   var today = new Date();
   var currentDay = today.getDay()
-  var day =""
-  switch (currentDay) {
-    case 0:
-      day ="Sunday"
-      break;
-    case 1:
-      day ="Monday"
-      break;
-    case 2:
-      day ="Tuesday"
-      break;
-    case 3:
-      day ="Wednesday"
-      break;
-    case 4:
-      day ="Thursday"
-      break;
-    case 5:
-      day ="Friday"
-      break;
-    case 6:
-      day ="Saturday"
-      break;
-    default:
-      console.log("error, current day is :" + currentDay)
-
+  var options = {
+    weekday :'long',
+    day : 'numeric',
+    month: 'long'
   }
-    res.render("list", {kindofDay:day})
+
+  var day = today.toLocaleDateString('en-us', options)
+  res.render("list", {kindofDay:day,newItem: item})
+})
+
+app.post("/", function(req,res){
+  var item= req.body.newItem
+  res.redirect("/")
 })
 
 app.listen(3000, function(){

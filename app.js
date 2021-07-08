c7e8e3f07c3e27adb633b06b504114eaac18963b
Severus11 <36445722+Severus11@users.noreply.github.com>
2021-07-08 13:03:19 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const bodyParser = require("body-parser")
+const date = require(__dirname +  "/date.js")
 
 const app = express()
 var items = ["buy food", "cook food"]
@@ -10,21 +11,17 @@ app.use(express.static('public'))
 
 app.set('view engine', 'ejs')
 app.get("/", function(req,res){
-  var today = new Date();
-  var currentDay = today.getDay()
-  var options = {
-    weekday :'long',
-    day : 'numeric',
-    month: 'long'
-  }
-
-  var day = today.toLocaleDateString('en-us', options)
+  let day = date.getDay()
   res.render("list", {listTitle:day,newItems: items})
 })
 
 app.get("/work", function(req,res){
   res.render("list", {listTitle: "Work List" , newItems: workitems})
 
+})
+
+app.get("/about", function(req,res){
+  res.render("about")
 })
 
 app.post("/work", function(req,res){
@@ -34,9 +31,17 @@ app.post("/work", function(req,res){
 })
 
 app.post("/", function(req,res){
-  var item= req.body.newItem
-  items.push(item)
-  res.redirect("/")
+  var item = req.body.newItem
+  if(req.body.list ==="Work List")
+  {
+    workitems.push(item)
+    res.redirect("/work")
+  }
+  else {
+      items.push(item)
+      res.redirect("/")
+  }
+
 })
 
 app.listen(3000, function(){
